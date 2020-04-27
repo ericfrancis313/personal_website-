be21c5be7a6f44b020385b5654dbf39d76df28d8
Eric <EricFrancis313@gmail.com>
2020-04-27 04:22:59 -0400

    added css grid/ gradient scroll

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,10 @@
+
+var nystories = document.querySelector("p").offsetTop;
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+ var opac = (window.pageYOffset / nystories);
+    console.log(opac);
+  document.body.style.background =  "linear-gradient(rgba(0, 0, 0, " + opac + "), rgba(0, 0, 0, " + opac + ")), url('IMG_6235-1.jpeg') no-repeat";
+  console.log(document.body.background)
+  }
+}
